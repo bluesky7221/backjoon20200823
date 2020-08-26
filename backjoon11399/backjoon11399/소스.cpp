c343b4fa3fa2c9fa2abdbe03e501736a72c97354
엄우철 <bluesky7221@naver.com>
2020-08-26 11:11:35 +0900

    ATM

--- backjoon11399/backjoon11399/소스.cpp
+++ backjoon11399/backjoon11399/소스.cpp
@@ -7,26 +7,48 @@
 #pragma warning(disable:4996)
 using namespace std;
 
+void partition(int* arr, int left, int right)
+{
+    if (left > right) return;
+    int pivot = left;
+    int low = left + 1;
+    int high = right;
+    while (true)
+    {
+        while (low <= right && arr[pivot] >= arr[low]) low++;
+        while (high >= left + 1 && arr[pivot] <= arr[high]) high--;
+        if (low >= high) break;
+        int temp0 = arr[low];
+        arr[low] = arr[high];
+        arr[high] = temp0;
+    }
+    int temp1 = arr[high];
+    arr[high] = arr[pivot];
+    arr[pivot] = temp1;
+    partition(arr, 0, high - 1);
+    partition(arr, high + 1, right);
+}
+
 int main() {
     int n;
     scanf("%d", &n);
 
-    vector<int> p;
+    int p[1000] = { 0 };
 
     for (int i = 0; i < n; i++)
     {
         int temp;
         scanf("%d", &temp);
 
-        p.push_back(temp);
+        p[i] = temp;
     }
 
-    sort(p.begin(), p.end(), less<int>());
+    partition(p, 0, n - 1);
     
     //x1*n + x2*(n-1) ...
     int sum = 0;
-    int j = p.size();
-    for (int i = 0; i < p.size(); i++)
+    int j = n;
+    for (int i = 0; i < n; i++)
     {
         sum += p[i] * j;
         j--;
